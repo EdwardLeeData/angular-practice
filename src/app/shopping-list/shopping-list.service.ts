d7d75fb240d2005570e628075afa7e7a63b2c97c
Jaeyun Lee <jaeyun.lee@dime.dev>
2023-05-05 12:30:27 -0500

    121

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,7 +1,10 @@
+import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 
 
 export class ShoppingListService{
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
     private ingredients: Ingredient[] = [
         new Ingredient('Celery', 2),
         new Ingredient('Carrots', 5)
@@ -13,5 +16,6 @@ export class ShoppingListService{
 
     addIngredients(ingredient: Ingredient){
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
