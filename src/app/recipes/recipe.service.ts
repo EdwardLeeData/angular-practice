1468d4bb9242c65009f8950f5a1be2d73e665eaf
Jaeyun Lee <jaeyun.lee@dime.dev>
2023-05-05 12:18:24 -0500

    119

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,21 +1,23 @@
-import { Recipe } from "./recipe.model";
-
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 export class RecipeService {
-    private recipes: Recipe[] = [
-        new Recipe(
-          'A Test Recipe',
-          'This is simply a test',
-          'https://www.wolflair.com/wp-content/uploads/2017/01/placeholder.jpg'
-        ),
-        new Recipe(
-          'Another Test Recipe',
-          'This is simply another test',
-          'https://www.wolflair.com/wp-content/uploads/2017/01/placeholder.jpg'
-        )
-      ];
+  recipeSelected = new EventEmitter<Recipe>();
 
-      getRecipes(){
-        return this.recipes.slice();
-      }
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.wolflair.com/wp-content/uploads/2017/01/placeholder.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply another test',
+      'https://www.wolflair.com/wp-content/uploads/2017/01/placeholder.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,23 +1,17 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipes-list',
-  templateUrl: './recipes-list.component.html'
+  templateUrl: './recipes-list.component.html',
 })
 export class RecipesListComponent {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService){};
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe);
   }
 }
